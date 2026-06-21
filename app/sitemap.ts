19559cd07cc2a8954707f03cf07dddfcc8149344
Muhammad Sufyan Jura <sufyanfaizan47@gmail.com>
2026-06-21 18:24:37 +0500

    Update sitemap URL for deployment

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://studymate.vercel.app",
+      url: "https://studymate-20.vercel.app/",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 1,
